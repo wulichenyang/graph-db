c3317abcf35fed06634753668b725293f411cb23
wulichenyang <943934049@qq.com>
2018-04-26 22:52:57 +0800

    finish relationshipState

--- kernel/api/txstate/RelationshipState.cpp
+++ kernel/api/txstate/RelationshipState.cpp
@@ -6,7 +6,19 @@ RelationshipState::RelationshipState()
 {
 }
 
+RelationshipState::RelationshipState(long id)
+	:PropertyContainerState(id)
+{
+}
+
 
 RelationshipState::~RelationshipState()
 {
+}
+
+void RelationshipState::setMetaData(long startNode, long endNode, int type)
+{
+	this->startNode = startNode;
+	this->endNode = endNode;
+	this->type = type;
 }
--- kernel/api/txstate/RelationshipState.h
+++ kernel/api/txstate/RelationshipState.h
@@ -1,8 +1,18 @@
 #pragma once
-class RelationshipState
+#include "TxState.h"
+#include "PropertyContainerState.h"
+
+class RelationshipState: public PropertyContainerState
 {
 public:
 	RelationshipState();
+	RelationshipState(long id);
 	~RelationshipState();
+	void setMetaData(long startNode, long endNode, int type);
+
+private:
+	long startNode = -1;
+	long endNode = -1;
+	int type = -1;
 };
 
